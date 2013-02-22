fb9d3a47e3cfc0ba6b5eece18578a816e7d96afe
Orest Ivasiv <halyph@gmail.com>
2013-02-22 11:47:18 +0200

    Added Javabrains tutorial project

---
+++ javabrains/src/main/java/org/hibernate/tutorials/App.java
@@ -0,0 +1,10 @@
+package org.hibernate.tutorials;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
