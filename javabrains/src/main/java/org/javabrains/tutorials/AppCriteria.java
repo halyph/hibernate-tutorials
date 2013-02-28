47e5a121dafa2d7d7de8da9fb4e59769708fee95
Orest Ivasiv <halyph@gmail.com>
2013-02-28 18:05:03 +0200

    Added Criteria, video 29

---
+++ javabrains/src/main/java/org/javabrains/tutorials/AppCriteria.java
@@ -0,0 +1,46 @@
+package org.javabrains.tutorials;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+import org.javabrains.tutorials.dto.BasicVehicle;
+
+import java.util.List;
+
+/**
+ * Demo AppUserDetails.
+ */
+public class AppCriteria {
+    public static void main(String[] args) {
+        System.out.println("BEGIN");
+
+        @SuppressWarnings("deprecation")
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Criteria criteria = session.createCriteria(BasicVehicle.class);
+        criteria.add(Restrictions.eq("vehicleName", "Mercedes1"));
+
+        List<BasicVehicle> basicVehicles = (List<BasicVehicle>) criteria.list();
+
+        printVehicleNames(basicVehicles);
+
+        session.getTransaction().commit();
+        session.close();
+
+
+    }
+
+
+    private static <T> void printVehicleNames(List<T> vehicleNames) {
+        for (T vehicleName : vehicleNames) {
+            System.out.println(vehicleName);
+        }
+    }
+
+}
